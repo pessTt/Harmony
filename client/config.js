b71112ded101ae83b228864a8b07014ea070ff07
Carlos Pessoa <carlosanpessoa@gmail.com>
2026-09-10 09:00:27 -0300

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,18 @@
+
+window.SERVER_URL = 'https://YOUR-SERVER-HERE.onrender.com';
+
+
+window.ICE_SERVERS = [
+  { urls: 'stun:stun.l.google.com:19302' },
+  { urls: 'stun:stun1.l.google.com:19302' },
+  {
+    urls: 'turn:openrelay.metered.ca:80',
+    username: 'openrelayproject',
+    credential: 'openrelayproject',
+  },
+  {
+    urls: 'turn:openrelay.metered.ca:443',
+    username: 'openrelayproject',
+    credential: 'openrelayproject',
+  },
+];
