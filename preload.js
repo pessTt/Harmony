b5da7498190d9067b62fae557d20a3187f832031
Carlos Pessoa <carlosanpessoa@gmail.com>
2026-09-10 08:51:46 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getScreenSources: () => ipcRenderer.invoke('get-screen-sources'),
-});
